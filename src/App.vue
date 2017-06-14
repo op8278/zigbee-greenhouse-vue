a6af0506a938bf6e7035b80dc6ca0560134e9349
op8278 <406386343@qq.com>
2017-06-14 21:09:43 +0800

    首次初始化项目

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <header>这是头部</header>
+    <div class="container">
+      <div class="tab">
+        <li>全部</li>
+        <li>温度</li>
+        <li>湿度</li>
+      </div>
+      <div class="canvas">显示图表地方</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
