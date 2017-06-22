ac742dab591deb347adeb8c3e365826ad051f27e
op8278 <406386343@qq.com>
2017-06-22 11:30:41 +0800

    修改组件引用

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Hello from '@/components/Hello';
 import Temperature from '@/components/temperature/Temperature.vue';
 import Humidity from '@/components/humidity/Humidity.vue';
 
