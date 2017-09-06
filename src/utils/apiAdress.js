d8bc9bad3f0e8cc5763cbd2423e6981d2bb25286
op8278 <406386343@qq.com>
2017-09-06 20:29:54 +0800

    Update apiAdress.js

--- src/utils/apiAdress.js
+++ src/utils/apiAdress.js
@@ -1,6 +1,4 @@
 //const baseUrl="你的上线服务器ip";  
-//const baseUrl="http://119.23.74.71:8080/api";
-//const wsUrl="ws://119.23.74.71:8080/websocket/zigbee";
 const baseUrl="http://192.168.1.103:8081/api";
 const wsUrl="ws://192.168.1.103:8081/websocket/zigbee";
 module.exports = {
